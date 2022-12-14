5cfa4f9168b28f43167315364a1fd6119172695d
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-12-14 16:01:37 -0600

    New Udemy course that I have started

---
+++ section-1/HelloJava.java
@@ -0,0 +1,13 @@
+public class HelloJava {
+    
+    public static void main(String[] args) {
+        System.out.println("RRRR   K   K");
+        System.out.println("R   R  K  K");
+        System.out.println("R   R  K K");
+        System.out.println("RRRR   KK");
+        System.out.println("R  R   K K");
+        System.out.println("R   R  K  K");
+        System.out.println("R    R K   K");
+    }
+
+}
---
+++ section-2/JoiningStrings.java
@@ -0,0 +1,10 @@
+public class JoiningStrings {
+    public static void main(String[] args) {
+        int year = 2010;
+        String winner = "Spain";
+
+        String announcement = "The winner of the world cup in " + year + " " + "was " + winner + " ";
+
+        System.out.print(announcement);
+    }
+}
