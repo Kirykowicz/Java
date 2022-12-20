6729a70c5eff1148f6b8e9ad6658a27978ccae45
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-12-20 16:28:03 -0600

    workbook 7.2 completed

---
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.2/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+ 
+public class Main {
+  
+    public static void main(String[] args) {
+        Person person = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
+ 
+        
+        person.passport = new String[]{person.name, person.nationality, person.dateOfBirth};
+        
+        
+        System.out.println("Name: " + person.name + "\n" + "Nationality: " + person.nationality + "\n" + "Date of Birth: " + person.dateOfBirth + "\n" + "Seat Number: " + person.seatNumber + "\n");
+    }
+  
+  
+}
