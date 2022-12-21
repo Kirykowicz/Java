145bc52b373e3fe9b3ce3e6f6d2cc2cc0d285126
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-12-21 12:43:55 -0600

    workbook 6.5 complete

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
@@ -0,0 +1,28 @@
+import java.nio.ReadOnlyBufferException;
+
+public class HighScore {
+    public static void main(String[] args) {
+        
+        int highScore = 0;
+        int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
+        
+        // Instructions for this workbook are on Learn the Part (Workbook 6.5).
+
+        System.out.print("\nHere are the scores: ");
+        for(int i = 0; i < scores.length; i++){
+            if(scores[i] > highScore){
+                highScore = scores[i];
+            }
+            System.out.print(scores[i] + " ");
+        }
+
+        System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
+        
+    }
+    
+    public static int randomNumber(){
+        double decimal = Math.random() * 50000;
+        return (int)decimal;
+    }
+
+}
