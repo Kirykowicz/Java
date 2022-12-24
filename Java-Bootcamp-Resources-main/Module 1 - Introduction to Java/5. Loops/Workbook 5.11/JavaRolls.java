5e6be3c93be06f58f739f0cc4599fea6ddbc929a
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-12-24 08:58:46 -0600

    workbook 5.11 complete

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.11/JavaRolls.java
@@ -4,14 +4,45 @@ public class JavaRolls {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // See Learn the Part for detailed instructions.
-        
-        int diceRoll;
+        System.out.println("Let's play Rolling Java. Type anything to start.");
+        scan.nextLine();
+        System.out.println("Great, here are the rules:\n");
+        System.out.println("- If you roll a 6, the game stops.");
+        System.out.println("- If you roll a 4, nothing happens.");
+        System.out.println("- Otherwise, you get 1 point.\n");
+        System.out.println("You must collect at least 3 points to win. Enter anything to roll:");
+
         int score = 0;
 
+        while(true){
+            int diceRoll = rollDice();
+            scan.nextLine();
+            System.out.print("You rolled a " + diceRoll + ".");
+
+            if(diceRoll == 6){
+                System.out.print(" End of game.");
+                break;
+            } else if(diceRoll == 4){
+                System.out.print(" Keep rolling.");
+            } else {
+                score++;
+                System.out.print(" One point. Keep rolling");
+            }
+        }
+
+        if(score >= 3){
+            System.out.println( "Wow, that's lucky. You win!");
+        }else{
+            System.out.println(" Tough luck, you lose :(");
+        }
+        
+        
+        
+        // scan.close();
     }
   
-  
-
-  
-  
+    public static int rollDice(){
+        double random = Math.random() * 6;
+        return (int)random + 1;
+    }
 }
