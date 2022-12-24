b718645f14c9e44e36d37de476bee6eb35bc2d1e
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-12-24 09:21:02 -0600

    workbook 5.8 completed

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 
 public class Guess  {
     public static void main(String[] args) {
-
+        int secretNumber = 3;
        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
         
        Scanner scan = new Scanner(System.in);
+
+       int guess = scan.nextInt();
+
+       while(guess != secretNumber){
+        System.out.print("Guess again ");
+        guess = scan.nextInt();
+       }
        
+       System.out.println("You got it!");
+
        //See Learn the Part for detailed instructions.
         
         scan.close();
